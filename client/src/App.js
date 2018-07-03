19f7d0824f497ee02fb4dc6e7f574fba013c41ee
ColePresnell <cole.presnell@gmail.com>
2018-07-03 10:17:32 -0500

    updated html

--- client/src/App.js
+++ client/src/App.js
@@ -17,19 +17,11 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <div>
-          Hello World!
-          <button onClick={this.getGetRequest}>GET</button>
-          <button onClick={this.getPostRequest}>POST</button>
-          </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+      <form>
+        <input id="title" />
+        <textarea id="post"></textarea>
+          </form>
       </div>
     );
   }
